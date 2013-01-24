210f2309aa4825b3a944647ddd7151b2bddcb44c
David DiGiovanni <davedg629@gmail.com>
2013-01-24 01:01:06 -0500

    Use current path of app in code, comment code

--- imagbg2.rb
+++ imagbg2.rb
@@ -1,21 +1,28 @@
 require 'json'
 require 'open-uri'
 
+# setup variables
 wallpaper = "";
 filename = "";
 body="";
+curr_path ="";
 
+# open sub reddit hot json feed and store response in body variable
 open("http://www.reddit.com/r/imaginarylandscapes/hot.json") { |f| body = f.read() }
 body = JSON.parse(body)
 
-wallpaper = body["data"]["children"][1]["data"]["url"]
+# extract the url of the first post
+wallpaper = body["data"]["children"][0]["data"]["url"]
 
+# get the file name from the url
 filename = wallpaper.split("/")
 filename = filename.last
 
-%x[wget -P ~/ruby/imaginarybgs/bg-images/ #{wallpaper}]
+# get current path
+curr_path = File.expand_path(File.dirname(__FILE__))
 
-%x[osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/David/ruby/imaginarybgs/bg-images/#{filename}"']
+# download file into bg-images directory
+%x[wget -P #{curr_path}/bg-images/ #{wallpaper}]
 
-#%x[defaults write com.apple.desktop '{ Background = {default = {ImageFilePath = "~/ruby/imaginarybgs/bg-image/#{filename}"; NewChangePath = "~/ruby/imaginarybgs/bg-image"; NewImageFilePath = "~/ruby/imaginarybgs/bg-image/#{filename}"; };};}']
-#%x[killall Dock]
+# change the desktop picture on mac using osascript
+%x[osascript -e 'tell application "System Events" to set picture of every desktop to "#{curr_path}/bg-images/#{filename}"']
